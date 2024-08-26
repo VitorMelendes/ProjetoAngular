cfca2aaeffd24ef0f012c4cf28c7c6d0c9a8b215
VitorMelendes <125466165+VitorMelendes@users.noreply.github.com>
2024-08-26 11:25:29 +0000

    app de media

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -6,8 +7,18 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+  constructor() { }
+ public notaA: number | undefined;
+ public notaB: number | undefined;
+ public media: number | undefined;
+ public situacao: string = "";
+ calcularMedia() {
+ if (this.notaA && this.notaB) {
+ this.media = (this.notaA + this.notaB) / 2;
+ this.situacao = this.media >= 7 ? 'Aprovado' : 'Reprovado';
+ }
+ }
 }
